943058aa2f123810fe3f0682ee77c5002998126e
zarko13 <zarsarenac@gmail.com>
2025-04-26 11:56:09 +0200

    first commit

---
+++ database/migrations/2025_04_26_095003_create_embeddings_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('embeddings', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('document_id');
+            $table->vector('embedding', 1536);
+            $table->json('metadata');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('embeddings');
+    }
+};
