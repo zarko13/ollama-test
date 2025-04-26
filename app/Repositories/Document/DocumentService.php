91c864e759fa79c5cdb07dedf57cb88731fae07d
zarko13 <zarsarenac@gmail.com>
2025-04-26 10:59:02 +0200

    first commit

---
+++ app/Repositories/Document/DocumentService.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Repositories\Document;
+
+use App\Models\Document;
+use App\Repositories\ServiceResponse;
+use Exception;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
+
+class DocumentService
+{
+
+    public static function storeDocument(string $name, $file){
+
+        $errors = null;
+        $data = [];
+
+        try {
+
+            $path = Storage::put('documents', $file);
+
+
+            $document = Document::create([
+                'name' => $name,
+                'path' => $path
+            ]);
+
+
+            $data['document'] = $document;
+
+        } catch (Exception $error) {
+            Log::error('Failed to store document.Error:'.$error);
+            $errors[] = 'Failed to store document';
+        }
+
+        return new ServiceResponse($errors, $data);
+
+    }
+
+    public static function deleteDocument(Document $document){
+
+        $errors = null;
+        $data = [];
+
+        try {
+
+
+            $document->delete();
+
+
+            $data['success'] = true;
+
+        } catch (Exception $error) {
+            Log::error('Failed to delete document.Error:'.$error);
+            $errors[] = 'Failed to delete document';
+        }
+
+        return new ServiceResponse($errors, $data);
+
+    }
+
+}
