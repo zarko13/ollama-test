468a53fb53158e763e5c1513e2a6ff907e8ab9c9
zarko13 <zarsarenac@gmail.com>
2025-04-26 12:29:31 +0200

    first commit

--- app/Models/ChatMessages.php
+++ app/Models/ChatMessages.php
@@ -3,8 +3,35 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ChatMessages extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $fillable = [
+        'chat_id',
+        'type',
+        'content'
+    ];
+
+    public static $_TYPE_BY_USER = 1;
+    public static $_TYPE_BY_SYSTEM = 2;
+
+    public function chat() : BelongsTo {
+        return $this->belongsTo(Chat::class);
+    }
+
+    public function toArray(){
+        $data = parent::toArray();
+
+        $data['is_by_user'] = $this->getIsByUserAttribute();
+
+        return $data;
+    }
+
+    public function getIsByUserAttribute(){
+        return $this->type == self::$_TYPE_BY_USER;
+    }
 }
