6a64d093b72d3c1f244c00f01cca5087e7c83367
zarko13 <zarsarenac@gmail.com>
2025-04-26 10:28:54 +0200

    first commit

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::firstOrCreate(
+            ['email' => 'admin@ollama.com'],
+            ['password' => Hash::make('test123'), 'name' => 'Admin']
+        );
+    }
+}
