93b16f74b5469b90abf2e4083c44c7e15affe6f2
zarko13 <zarsarenac@gmail.com>
2025-05-01 11:47:15 +0200

    working

--- resources/js/Pages/Bot/Index.vue
+++ resources/js/Pages/Bot/Index.vue
@@ -54,7 +54,9 @@
                                     {{ bot.model }}
                                 </td>
                                 <td class="px-6 py-4">
-
+                                    <a :href="'/bot/' + bot.id ">
+                                        <button type="button" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Details</button>
+                                    </a>
                                 </td>
                             </tr>
 
